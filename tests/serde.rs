0986c4ac8243156993033ba3f1edd9f5151f3dae
Fedeparma74 <fedeparma74@gmail.com>
2024-02-04 17:58:12 +0100

    Add serde integration test

---
+++ tests/serde.rs
@@ -0,0 +1,45 @@
+#![cfg(test)]
+#![cfg(all(target_family = "wasm", feature = "serde"))]
+
+use wasm_bindgen_test::wasm_bindgen_test;
+use web_time::SystemTime;
+
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+/// (De)serialization of [`SystemTime`].
+async fn system_time() {
+	let time = SystemTime::now();
+	let serialized = serde_json::to_string(&time).unwrap();
+	let deserialized: SystemTime = serde_json::from_str(&serialized).unwrap();
+	assert_eq!(time, deserialized);
+}
+
+#[wasm_bindgen_test]
+/// (De)serialization of [`SystemTime`] with [`UNIX_EPOCH`].
+async fn unix_epoch() {
+	let time = SystemTime::UNIX_EPOCH;
+	let serialized = serde_json::to_string(&time).unwrap();
+	let deserialized: SystemTime = serde_json::from_str(&serialized).unwrap();
+	assert_eq!(time, deserialized);
+}
+
+#[wasm_bindgen_test]
+/// (De)serialization compatibility with [`std::time::SystemTime`].
+async fn std_compatibility() {
+	let time = SystemTime::now();
+	let serialized = serde_json::to_string(&time).unwrap();
+	let deserialized: std::time::SystemTime = serde_json::from_str(&serialized).unwrap();
+	assert_eq!(
+		time.duration_since(SystemTime::UNIX_EPOCH).unwrap(),
+		deserialized.duration_since(std::time::UNIX_EPOCH).unwrap()
+	);
+
+	let time = std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(1_000_000);
+	let serialized = serde_json::to_string(&time).unwrap();
+	let deserialized: SystemTime = serde_json::from_str(&serialized).unwrap();
+	assert_eq!(
+		time.duration_since(std::time::UNIX_EPOCH).unwrap(),
+		deserialized.duration_since(SystemTime::UNIX_EPOCH).unwrap()
+	);
+}
